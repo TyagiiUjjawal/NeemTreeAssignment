61abb4af6d3afae5f13af4f204d556d39f32c284
Ujjawal_Tyagi <tyagiujjawal9@gmail.com>
2023-12-22 13:08:30 +0530

    ds

--- controller/uploadController.js
+++ controller/uploadController.js
@@ -2,7 +2,8 @@ const multer = require("multer");
 const xlsx = require("xlsx");
 const path = require("path");
 const Candidate = require("../models/candidateModel");
-
+const { log } = require("console");
+const async = require("async");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
@@ -17,11 +18,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+// const async = require("async");
+
 const handleUpload = async (req, res) => {
   try {
     if (!req.file) {
       return res.status(400).send("No file uploaded.");
     }
+    console.log(req.file.path);
     const workbook = xlsx.readFile(req.file.path);
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
@@ -30,28 +34,52 @@ const handleUpload = async (req, res) => {
       return res.status(400).send("No data found in the Excel file.");
     }
 
-    for (const candidate of excelData) {
-      const existingCandidate = await Candidate.findOne({
-        email: candidate.email,
-      });
+    async.eachSeries(
+      excelData,
+      async (candidate) => {
+        try {
+          const existingCandidate = await Candidate.findOne({
+            email: candidate.email,
+          });
+          console.log(
+            "candidate",
+            candidate,
+            "existing cand",
+            existingCandidate
+          );
 
-      if (!existingCandidate) {
-        const newCandidate = new Candidate({
-          name: candidate.name,
-          email: candidate.email,
-        });
-        await newCandidate.save();
-      } else {
-        const messagee = {
-          message: "email already exist",
-        };
-        return res.render("landing", messagee);
+          if (!existingCandidate) {
+            const newCandidate = new Candidate({
+              name: candidate.name,
+              email: candidate.email,
+            });
+            console.log("saving");
+            await newCandidate.save();
+            const message = {
+              message: "Thank you <br> File successfully uploaded",
+            };
+            console.log("File successfully uploaded");
+            return res.render("landing", message);
+          } else {
+            console.log("already exist");
+            const message = {
+              message: "Email already exists",
+            };
+            console.log("Email already exists");
+            return res.render("landing", message);
+          }
+        } catch (err) {
+          console.error(err);
+          return res.status(500).send("Error processing Excel file");
+        }
+      },
+      (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Error processing Excel file");
+        }
       }
-    }
-    const messagee = {
-      message: "Thank you <br> File successfully uploaded",
-    };
-    return res.render("landing", messagee);
+    );
   } catch (error) {
     console.error(error);
     return res.status(500).send("Error processing Excel file");
